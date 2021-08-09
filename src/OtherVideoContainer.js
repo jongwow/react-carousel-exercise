a1fa29d866a0853816abb6993807d13098246eb2
JongWoo Park <jjongwoo.dev@gmail.com>
2021-08-09 15:28:09 +0900

    migrate: 최신 디자인 반영

--- src/OtherVideoContainer.js
+++ src/OtherVideoContainer.js
@@ -2,48 +2,58 @@ import { CarouselLeft, CarouselRight } from "./carousel";
 import VideoBox from "./VideoBox";
 
 function OtherVideoContainer(props) {
-    const {
-      activePlayer,
-      cameraViewNumber,
-      isOver,
-      addActiveIndex,
-      subActiveIndex,
-    } = props;
-    let activePlayerList = Object.keys(activePlayer.playerToDist);
-    return (
+  const {
+    activePlayer,
+    cameraViewNumber,
+    isOver,
+    addActiveIndex,
+    subActiveIndex,
+  } = props;
+  let activePlayerList = Object.keys(activePlayer.playerToDist);
+  return (
+    <div
+      id="video-container"
+      style={{
+        position: `absolute`,
+        top: `50px`,
+        left: `50%`,
+        transform: `translateX(-50%)`,
+        height: "130px",
+        width: `${cameraViewNumber * 196 + 80}px`,
+        backgroundColor: "yellow",
+      }}
+    >
       <div
-        id="video-container"
+        id="other-video-wrapper"
         style={{
-          position: `absolute`,
-          top: `50px`,
-          left: `50%`,
-          transform: `translateX(-50%)`,
-          height: "130px",
-          width: `${cameraViewNumber * 196 + 80}px`,
-          backgroundColor: "yellow",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        <div
-          id="other-video-wrapper"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          {isOver && <CarouselLeft onClick={subActiveIndex}/>}
-          {activePlayerList.map((playerId, idx) => (
+        {isOver && <CarouselLeft onClick={subActiveIndex} />}
+        {activePlayerList.map((playerId, idx) => (
+          <div
+            key={playerId}
+            style={{
+              width: "180px",
+              height: "120px",
+              margin: "0 8px",
+            }}
+          >
             <VideoBox
-              key={playerId}
               text={`${playerId}:${activePlayer.playerToDist[playerId]}`}
               style={
-                idx !== activePlayerList.length - 1 ? { marginRight: "16px" } : {}
+                idx !== activePlayerList.length - 1
+                  ? { marginRight: "16px" }
+                  : {}
               }
             />
-          ))}
-          {isOver && <CarouselRight onClick={addActiveIndex}/>}
-        </div>
+          </div>
+        ))}
+        {isOver && <CarouselRight onClick={addActiveIndex} />}
       </div>
-    );
-  }
-  
-  export default OtherVideoContainer;
+    </div>
+  );
+}
+
+export default OtherVideoContainer;
--- src/VideoBox.js
+++ src/VideoBox.js
@@ -1,21 +1,101 @@
-import {randomColor} from './randomId';
+import { useState } from "react";
+import { randomColor } from "./randomId";
 
 function VideoBox({ text, style }) {
-    let color = randomColor(text[0]);
-    return (
+  const [showMenu, setShowMenu] = useState(false);
+  let color = randomColor(text[0]);
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        borderRadius: "20px",
+        backgroundColor: color,
+      }}
+      onMouseLeave={() => setShowMenu(false)}
+      onMouseEnter={() => setShowMenu(true)}
+    >
       <div
         style={{
-          // backgroundColor: `#${text.substring(0, 6)}`,
-          backgroundColor: color,
           width: "180px",
           height: "120px",
-          borderRadius: `20px`,
-          ...style,
+          objectFit: "cover",
         }}
       >
-        {text}
+        video
       </div>
-    );
-  }
 
-  export default VideoBox;
+      {showMenu && <GameVideoMenu />}
+      <GameVideoName name={text} />
+    </div>
+  );
+}
+function GameVideoMenu() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: `translate(-50%, -50%)`,
+        backgroundColor: "pink",
+      }}
+    >
+      <div style={{ marginRight: "12px" }}>{micOn}</div>
+      <div style={{ marginRight: "12px" }}>{videoOn}</div>
+      <div>{fullScreen}</div>
+    </div>
+  );
+}
+
+function GameVideoName({ name }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: "8px",
+        right: "8px",
+        textOverflow: "ellipsis",
+        height: "23px",
+        borderRadius: "12px",
+        padding: "4px 6px",
+        backgroundColor: "rgba(0,0,0,0.6)",
+        color: "white",
+        fontSize: "12px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {name}
+    </div>
+  );
+}
+
+export default VideoBox;
+
+const micOn = (
+  <img
+    style={{ width: "32px", height: "32px" }}
+    alt="mic_on"
+    src={require("./images/v1/mic_on.png").default}
+  />
+);
+const videoOn = (
+  <img
+    style={{ width: "32px", height: "32px" }}
+    alt="video_on"
+    src={require("./images/v1/video_on.png").default}
+  />
+);
+const fullScreen = (
+  <img
+    style={{ width: "32px", height: "32px" }}
+    alt="full"
+    src={require("./images/v1/full.png").default}
+  />
+);
